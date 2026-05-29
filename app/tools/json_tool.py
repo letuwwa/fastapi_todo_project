8133ce7ac92a82354ffbb7d85990bd1c2015dc46
Max Litvin <letuwwa@gmail.com>
2026-05-29 14:47:44 +0300

    feat: users and tasks split

--- app/tools/json_tool.py
+++ app/tools/json_tool.py
@@ -12,8 +12,21 @@ class JSONTool:
         except FileNotFoundError:
             return {}
 
-    def write(self, data: dict):
+    def write_user(self, data: dict):
         past_data = self.read()
         past_data.update(data)
+        with open(self.filename, "w") as f:
+            json.dump(past_data, f, indent=4)
+
+    def write_task(self, data: dict):
+        past_data = self.read()
+        if not past_data or past_data.get(data["username"]) is None:
+            past_data[data["username"]] = [
+                {"id": data["id"], "description": data["description"], "is_done": data["is_done"]},
+            ]
+        else:
+            past_data[data["username"]].append(
+                {"id": data["id"], "description": data["description"], "is_done": data["is_done"]},
+            )
         with open(self.filename, "w") as f:
             json.dump(past_data, f, indent=4)
