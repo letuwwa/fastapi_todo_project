c5023aec3b0bc54c79c2be7ee25d0e3392ab6fb4
Max Litvin <letuwwa@gmail.com>
2026-05-30 13:35:00 +0300

    fix: rb and wb keys

--- app/tools/json_tool.py
+++ app/tools/json_tool.py
@@ -7,16 +7,20 @@ class JSONTool:
 
     def read(self):
         try:
-            with open(self.filename, "r") as f:
+            with open(self.filename, "rb") as f:
                 return json.load(f)
         except FileNotFoundError:
             return {}
 
+    def write(self, data: dict):
+        with open(self.filename, "wb") as f:
+            json_data = json.dumps(data, indent=4)
+            f.write(json_data.encode("utf-8"))
+
     def write_user(self, data: dict):
         past_data = self.read()
         past_data.update(data)
-        with open(self.filename, "w") as f:
-            json.dump(past_data, f, indent=4)
+        self.write(past_data)
 
     def write_task(self, data: dict):
         past_data = self.read()
@@ -36,16 +40,14 @@ class JSONTool:
                     "is_done": data["is_done"],
                 },
             )
-        with open(self.filename, "w") as f:
-            json.dump(past_data, f, indent=4)
+        self.write(past_data)
 
     def delete_task(self, username: str, task_id: str):
         past_data = self.read()
         past_data[username] = [
             task for task in past_data[username] if task["task_id"] != task_id
         ]
-        with open(self.filename, "w") as f:
-            json.dump(past_data, f, indent=4)
+        self.write(past_data)
 
     def update_task_done(self, username: str, task_id: str):
         past_data = self.read()
@@ -54,5 +56,4 @@ class JSONTool:
                 if task["task_id"] == task_id:
                     task["is_done"] = True
                     break
-            with open(self.filename, "w") as f:
-                json.dump(past_data, f, indent=4)
+            self.write(past_data)
